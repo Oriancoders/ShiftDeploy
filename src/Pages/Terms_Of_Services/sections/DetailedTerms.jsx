be3a8be7bedcbda1b9c95ee841fb869ea32f91fa
SamiiiU <sammiiiullah@gmail.com>
2025-10-23 18:59:00 +0500

    pushed terms 9

---
+++ src/Pages/Terms_Of_Services/sections/DetailedTerms.jsx
@@ -0,0 +1,223 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const DetailedTerms = () => {
+  return (
+    <section className='bg-gray-50 overflow-hidden flex flex-col items-center justify-center pb-20'>
+      <div className='max-w-7xl mx-auto px-6  relative z-10 mt-10'>
+        <section id='P_1' className='space-y-4 mb-10'>
+          <h1 className='text-3xl font-bold text-primaryBlue'>1) How ShiftDeploy Operates
+          </h1>
+          <p>
+            Welcome to ShiftDeploy. These Terms of Service explain the rules that apply
+            when you visit our website, engage with our services, or collaborate with
+            our team. ShiftDeploy is a technology and development partner that builds,
+            maintains, and supports custom software, web applications, and cloud-based
+            solutions for businesses around the world.
+          </p>
+          <p>
+            By using our website or services, you agree to follow these terms. If you
+            do not agree, please do not use our platform or enter into any engagement
+            with us.
+          </p>
+
+        </section>
+
+        <section id='P_2' className='space-y-4 mb-10'>
+          <h1 className='text-3xl font-bold text-primaryBlue'>2) When These Terms Apply
+          </h1>
+          <p>
+            These Terms apply to all visitors of our website, prospective clients,
+            existing clients, and any entity or individual interacting with ShiftDeploy
+            in relation to our services. They govern both your browsing of our site and
+            your participation in any service agreement or digital engagement we
+            provide.
+          </p>
+          <p>
+            Certain projects or collaborations may include additional contracts or
+            statements of work. In those cases, those agreements take precedence over
+            these general Terms where conflicts exist.
+          </p>
+
+        </section>
+
+        <section id='P_3' className='space-y-4 mb-10'>
+          <h1 className='text-3xl font-bold text-primaryBlue'>3) Who Can Use Our Services
+          </h1>
+          <p>
+            Our services are intended for business use by individuals aged 18 or older
+            who are authorized to represent an organization or act on their own behalf.
+            By accessing our website or requesting services, you confirm that you meet
+            these eligibility requirements and are legally capable of entering into a
+            binding agreement.
+          </p>
+          <p>
+            ShiftDeploy reserves the right to restrict access or terminate engagements
+            if we believe a user has provided false information, is acting unlawfully,
+            or is misusing our website or services.
+          </p>
+
+        </section>
+
+        <section id='P_4' className='space-y-4 mb-10'>
+          <h1 className='text-3xl font-bold text-primaryBlue'>4) Proper Use of Our Website
+          </h1>
+          <p>
+            You agree to use ShiftDeploy’s website and digital platforms responsibly.
+            You may not attempt to disrupt, modify, or exploit the site in any way that
+            could harm its operation or other users. This includes attempting to gain
+            unauthorized access to our systems, reverse engineering code, or using any
+            automated tools (such as bots or scrapers) without explicit permission.
+          </p>
+          <p>
+            All information and materials provided on our site are for general
+            informational purposes. Any reliance on this content is at your own risk.
+          </p>
+
+        </section>
+
+        <section id='P_5' className='space-y-4 mb-10'>
+          <h1 className='text-3xl font-bold text-primaryBlue'>5) Your Account & Obligations
+          </h1>
+          <p>
+            In some cases, you may need to create an account or provide access
+            credentials to collaborate with ShiftDeploy. You are responsible for
+            maintaining the confidentiality of your login details and for all activity
+            that occurs under your account.
+          </p>
+          <p>
+            You agree to notify ShiftDeploy immediately of any unauthorized use or
+            security breach. We are not liable for any loss resulting from failure to
+            protect your credentials or comply with these obligations.
+          </p>
+          <p>
+            ShiftDeploy may suspend or terminate account access if we detect misuse,
+            fraudulent activity, or a violation of these Terms.
+          </p>
+
+        </section>
+
+        <section id='P_6' className='space-y-4 mb-10'>
+          <h1 className='text-3xl font-bold text-primaryBlue'>6) How Projects Are Engaged
+          </h1>
+          <p>
+            Every project or engagement with ShiftDeploy begins with a shared
+            understanding of scope, deliverables, and timelines. These details are
+            typically defined in a written proposal, statement of work (SOW), or
+            service agreement.
+          </p>
+          <p>
+            By confirming a project, you acknowledge and agree that:
+          </p>
+          <ul className="list-disc pl-6 mt-2 space-y-1 ">
+            <li>All deliverables and deadlines are based on mutual agreement and communication.</li>
+            <li>Client feedback and approvals are essential to maintaining timelines.</li>
+            <li>Scope changes may require an updated estimate or revised delivery schedule.</li>
+          </ul>
+          <p>
+            We value transparent collaboration and always aim to document major
+            decisions and project changes clearly.
+
+          </p>
+
+
+        </section>
+
+        <section id='P_7' className='space-y-4 mb-10'>
+          <h1 className='text-3xl font-bold text-primaryBlue'>7) Payments, Refunds & Billing
+          </h1>
+          <p>
+            ShiftDeploy operates on a transparent billing structure. Payment terms and
+            schedules are outlined in each client agreement or invoice.
+          </p>
+          <p>
+            By confirming a project, you acknowledge and agree that:
+          </p>
+          <ul className="list-disc pl-6 mt-2 space-y-1 ">
+            <li><strong>Invoices:</strong> Issued upon project initiation, milestone completion, or delivery — as defined in the engagement agreement.</li>
+            <li><strong>Payment Methods:</strong> We accept bank transfers, online payments, or other approved channels.</li>
+            <li><strong>Refunds:</strong> We refund full amount if client is not satisfied with milestone 1.  Generally not available after Milestons 1 is approved, except where legally required or explicitly stated.</li>
+            <li><strong>Late Payments :</strong> May incur a reasonable service fee or delay in project progress.</li>
+          </ul>
+          <p>
+            For subscription-based or retainer services, clients will receive notice
+            before any recurring billing renewal.
+          </p>
+        </section>
+
+        <section id='P_8' className='space-y-4 mb-10'>
+          <h1 className='text-3xl font-bold text-primaryBlue'>8) Timelines, Revisions & Delivery
+          </h1>
+          <p>
+            ShiftDeploy aims to meet all mutually agreed deadlines. Project timelines
+            depend on both technical factors and client responsiveness.
+          </p>
+          <ul className="list-disc pl-6 mt-2 space-y-1 ">
+            <li>All estimated delivery dates assume timely client feedback and content submission.</li>
+            <li>Minor revisions are typically included within the defined scope.</li>
+            <li>Significant changes requested after approval may result in revised estimates or schedules.</li>
+          </ul>
+          <p>
+            Once final deliverables are handed over, clients are responsible for
+            reviewing and confirming acceptance within the agreed review period.
+          </p>
+        </section>
+
+
+        <section id='P_9' className='space-y-4 mb-10'>
+          <h1 className='text-3xl font-bold text-primaryBlue'>9) Ownership & Intellectual Rights
+          </h1>
+          <p>
+            Unless otherwise stated in writing, all intellectual property created by
+            ShiftDeploy during a client project — including but not limited to software
+            code, designs, documentation, and technical strategies — remains the
+            property of ShiftDeploy until full payment is received
+          </p>
+
+          <p>
+            Once the project is fully paid for, ownership of the final deliverables
+            explicitly defined in the agreement transfers to the client. However,
+            ShiftDeploy retains rights to:
+          </p>
+          <ul className="list-disc pl-6 mt-2 space-y-1">
+            <li>Reuse or adapt general development frameworks and internal tools.</li>
+            <li>Maintain ownership of pre-existing libraries or proprietary components used in delivery.</li>
+            <li>Showcase non-confidential parts of the project in our portfolio or case studies, with client permission where applicable.</li>
+          </ul>
+          <p>
+             Clients agree not to reproduce, redistribute, or sublicense any ShiftDeploy
+    intellectual property beyond the terms of the agreement without written consent.
+          </p>
+        </section>
+
+        <hr className="border-t border-gray-200 my-8" />
+
+        <p className="text-sm text-gray-500 mt-2 text-center">
+          Last updated on <span className="font-medium text-gray-700">October 17, 2025</span>
+        </p>
+
+        <section className="bg-gray-50 mt-10 p-6 rounded-2xl text-center">
+          <h3 className="text-3xl font-semibold text-primaryBlue mb-3">
+            Have Questions About How We Handle Data?
+          </h3>
+          <p className="text-gray-600 mb-5 max-w-xl mx-auto">
+            ShiftDeploy is committed to clear communication and responsible data practices.
+            If you’d like to learn more about how we protect your information, we’d be glad to help.
+          </p>
+          <Link
+            to={"/ContactUs"}
+            className="inline-block hover:bg-toOrange text-white font-semibold px-6 py-3 rounded-lg bg-primaryOrange transition-colors"
+          >
+            Contact Our Team
+          </Link >
+        </section>
+
+
+      </div>
+
+
+    </section>
+  )
+}
+
+export default DetailedTerms
