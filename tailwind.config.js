872c3a95c3001ac6385f50482a639e8ebf342544
SamiiiU <sammiiiullah@gmail.com>
2026-02-17 02:19:40 +0500

    pushed changes audit 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        // This tells Tailwind: "When I use 'font-sans' (default), look for Inter first."
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
       colors : {
         primaryBlue : '#0C1F3A',
         toBlue : '#0B1D30',
